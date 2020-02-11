86681fc949b738056ef8de7f60a3df1471b774d8
Kanaan Irwin <Kanaanci@yahoo.com>
2020-02-11 15:35:27 -0600

    Update
    Update commit

---
+++ src/com/leetcode/easy_problems/ReverseLinkedList.java
@@ -0,0 +1,18 @@
+package com.leetcode.easy_problems;
+
+//Linked list definition
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+public class ReverseLinkedList {
+    public static ListNode reverseList(ListNode head){
+        
+
+    }
+}
